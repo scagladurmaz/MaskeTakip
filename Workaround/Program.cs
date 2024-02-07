c1b5ce80f5ff08994ec3e778c029b63abe0ea570
scagladurmaz <scagladurmaz@gmail.com>
2024-02-07 14:14:23 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,13 @@
+﻿ // See https://aka.ms/new-console-template for more information
+
+
+string mesaj = "Merhaba!";
+double tutar = 100000; //dbden gelecek değer
+int sayi = 100;
+bool  ;
+
+for(int i=0; i<mesaj.Length; i++)
+
+Console.WriteLine(mesaj);
+
+Console.ReadLine();
